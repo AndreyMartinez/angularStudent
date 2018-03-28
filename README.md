a38f7d59289b9fa8bb694cf13b89a61e4d87ca5b
rmartinez <rmartinez@conexia.com>
2018-03-28 16:44:19 -0500

    Add text readme project

---
+++ README.md
@@ -0,0 +1,14 @@
+# ANGULAR 4 , ANGULAR 5 COURSE UDEMY
+<hr>
+<p>En este proyecto se puede ver reflejado un poco el conocimiendo adquirido frente a temas de: </p>
+<ol>
+<li>
+TYPESCRIPT
+</li>
+<li>
+COMPONENT
+</li>
+<li>
+GENERATE PROJECT ANGULAR CLI 
+</li>
+</ol>
--- angular0.0.1/src/app/elementOne/element.component.ts
+++ angular0.0.1/src/app/elementOne/element.component.ts
@@ -9,4 +9,18 @@ import { Component } from '@angular/core'
 export class ElementComponent{
 public numberComponent = "this is my new component";
 public trabajos:Array<number>=[1,2,3,7]
+
+constructor(){
+}
+
+ngOnInit(){
+    this.functionExample("raphael");
+
+}
+
+functionExample(data){
+    setTimeout(()=>{
+        alert("ok "+data.toUpperCase())
+    },3000)
+}
 }
